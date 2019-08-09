53d1efc2f056faa7d942a25c648f9040965f78b8
Deepak Bhari <deepakbhari@Deepaks-MBP.lan>
2019-08-09 10:27:53 +0100

    convert winsAnalysisWithHtmlReport to a static method, so do'nt need to create an instance of Summary

--- build/index.js
+++ build/index.js
@@ -2,14 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
 var CsvFileReader_1 = require("./CsvFileReader");
-var winsAnalysis_1 = require("./Analyzers/winsAnalysis");
 var Summary_1 = require("./Summary");
-var HtmlReport_1 = require("./ReportTargets/HtmlReport");
 //create an object that satisfies the 'DataReader' interface
 var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 //create an instance of MatchReader and pass in something satisfying
 //the 'DataReader' interface
 var matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
-var summary = new Summary_1.Summary(new winsAnalysis_1.WinsAnalysis('Man United'), new HtmlReport_1.HtmlReport());
+//const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+var summary = Summary_1.Summary.winsAnalysisWithHtmlReport('Liverpool');
 summary.buildAndPrintReport(matchReader.matches);
--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+//const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+const summary = Summary.winsAnalysisWithHtmlReport('Liverpool');
 
 summary.buildAndPrintReport(matchReader.matches);
