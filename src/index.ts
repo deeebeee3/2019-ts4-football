d9b678528cc353994dc9bb404bb24a29a6eff052
Deepak Bhari <deepakbhari@Deepaks-MBP.lan>
2019-08-09 10:38:36 +0100

    add a static method to MatchReader so no longer need to instantiate in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,8 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
-import { ConsoleReport } from './ReportTargets/ConsoleReport';
-import { WinsAnalysis } from './Analyzers/winsAnalysis';
 import { Summary } from './Summary';
-import { HtmlReport } from './ReportTargets/HtmlReport';
 
-//create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
-
-//create an instance of MatchReader and pass in something satisfying
-//the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
 
-//const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 const summary = Summary.winsAnalysisWithHtmlReport('Liverpool');
-
 summary.buildAndPrintReport(matchReader.matches);
